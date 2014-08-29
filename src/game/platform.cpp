24d65fbd450db2baf280386eb8d0e07d6061a7d1
Ben Anderson <benanderson@acidic.co.nz>
2014-08-29 12:25:58 +1200

    Game now loads the config file and init's the resource manager correctly

--- src/game/platform.cpp
+++ src/game/platform.cpp
@@ -13,26 +13,6 @@ class InitState : public gegame::State {
 };
 
 int main() {
-	gecom::Config config_test("res/config/PlatformGame.json");
-	// int jump_key = config.get("keybindings").get("jump");
-	int jump_key = config_test.get("keybindings")->get_int("jump");
-	std::cout << "Jump key is: " << jump_key << std::endl;
-	// int jump_key = config.get("keybingings.jump");
-
-
-	ResourceManager rm("res/");
-	rm.add_cache_file<Text>("example.txt");
-
-	while(rm.cache()) ;
-
-	std::shared_ptr<Text> text_file = rm.get<Text>("example.txt");
-	std::cout << text_file->data() << std::endl;
-
 	gegame::Game platformGame;
 	platformGame.run(new InitState);
-
-	// Test
-	gegame::createWindow().size(1024, 768).title("Golden Eagle").visible(true);
-
-	while(true) ;
 }
--- src/gegame/Game.hpp
+++ src/gegame/Game.hpp
@@ -5,12 +5,31 @@
 
 #include "State.hpp"
 #include "ResourceManager.hpp"
+#include "Scene.hpp"
+#include "ge_common/Config.hpp"
 
 namespace gegame {
 	class Game {
-		// ResourceManager resorces;
+		ResourceManager* resources;
+		gecom::Config* game_config;
+
+		// To force clang to try and compile Scene
+		Scene test_scene;
 	public:
-		void run(State* initial_state) { }
+		void run(State* initial_state) { 
+			game_config = new gecom::Config("config.json"); // Should this be 
+															// done without a 
+															// hardcode - if so
+													 		// how?
+
+			std::string res_prefix = game_config
+										->get("system")
+										->get_string("resources_directory");
+
+			resources = new ResourceManager(res_prefix);
+
+			// here, we should push the initial state to the stack.
+		}
 	};
 }
 
