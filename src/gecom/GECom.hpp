afdb8b4eab0407f5de403f6ba463c7947fb97c6b
Ben Allen <benos.9k@gmail.com>
2014-09-06 17:13:50 +1200

    fix gecom::trim() for gcc/clang

--- src/gecom/GECom.hpp
+++ src/gecom/GECom.hpp
@@ -36,9 +36,9 @@ namespace gecom {
 	
 	// trim leading and trailing whitespace
 	inline std::string trim(const std::string &s) {
-	   auto wsfront = std::find_if_not(s.begin(), s.end(), std::isspace);
-	   auto wsback = std::find_if_not(s.rbegin(), s.rend(), std::isspace).base();
-	   return wsback <= wsfront ? std::string() : std::string(wsfront, wsback);
+		auto wsfront = std::find_if_not(s.begin(), s.end(), [](int c) { return std::isspace(c); });
+		auto wsback = std::find_if_not(s.rbegin(), s.rend(), [](int c) { return std::isspace(c); }).base();
+		return wsback <= wsfront ? std::string() : std::string(wsfront, wsback);
 	}
 }
 
